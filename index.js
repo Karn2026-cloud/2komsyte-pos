589830841c54cc57ad39499fc5cf8e33526fab4a
Karn2026-cloud <karanindrale253@gmail.com>
2025-08-29 09:44:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,12 @@ app.use((req, res, next) => {
   return express.json()(req, res, next);
 });
 
-const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173','https://komsyte-pos-frontend-2-git-main-komsyte-poss-projects.vercel.app', process.env.FRONTEND_URL].filter(Boolean);
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5173',
+  process.env.FRONTEND_URL,
+  'https://komsyte-pos-frontend-2-git-main-komsyte-poss-projects.vercel.app'
+].filter(Boolean);
 app.use(cors({
   origin: (origin, callback) => {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -494,3 +499,4 @@ app.get('/api/bills', authMiddleware, async (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
